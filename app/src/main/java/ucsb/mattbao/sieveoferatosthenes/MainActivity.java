152b8c6af078f4abaff5d786f47fd16c2e89c615
Matt Bao <bobconan@gmail.com>
2015-04-23 01:04:06 -0700

    Added user input check: if input is empty, display alert dialog, and provide a option to randomly generate number for user.

--- app/src/main/java/ucsb/mattbao/sieveoferatosthenes/MainActivity.java
+++ app/src/main/java/ucsb/mattbao/sieveoferatosthenes/MainActivity.java
@@ -1,6 +1,8 @@
 package ucsb.mattbao.sieveoferatosthenes;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,6 +11,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -27,10 +31,32 @@ public class MainActivity extends ActionBarActivity {
         goButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                upperLimit = Integer.valueOf(upperLimitInput.getText().toString());
-                Intent intent = new Intent(getBaseContext(), DisplayPrimeListActivity.class);
-                intent.putExtra("upperLimit", upperLimit);
-                startActivity(intent);
+                if(upperLimitInput.getText().length()>0){ // If input is not empty
+                    upperLimit = Integer.valueOf(upperLimitInput.getText().toString());
+                    Intent intent = new Intent(getBaseContext(), DisplayPrimeListActivity.class);
+                    intent.putExtra("upperLimit", upperLimit);
+                    startActivity(intent);
+                } else { // Displays alert dialog if input is empty
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle(R.string.alert_title).setMessage(R.string.alert_message);
+                    builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //Automatically dismisses alert dialog
+                        }
+                    });
+                    builder.setNeutralButton(R.string.generate_random, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Random random = new Random();
+                            //Set input to be random number between 2 and 1000002
+                            upperLimitInput.setText(String.valueOf(random.nextInt(1000000)+2));
+                            dialog.dismiss();
+                        }
+                    });
+                    builder.create().show();
+                }
+
             }
         });
     }
