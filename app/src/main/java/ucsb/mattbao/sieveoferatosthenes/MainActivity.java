7ed6bb3017ee0cab088c586c21f52a2be7a66536
Matt Bao <bobconan@gmail.com>
2015-04-23 22:22:09 -0700

    Added input-check for numbers over 15000000; also moved alert dialog building process in their respective methods to keep onCreate short

--- app/src/main/java/ucsb/mattbao/sieveoferatosthenes/MainActivity.java
+++ app/src/main/java/ucsb/mattbao/sieveoferatosthenes/MainActivity.java
@@ -31,36 +31,69 @@ public class MainActivity extends ActionBarActivity {
         goButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(upperLimitInput.getText().length()>0){ // If input is not empty
+                if(upperLimitInput.getText().length() > 0){ // If input is not empty
                     upperLimit = Integer.valueOf(upperLimitInput.getText().toString());
-                    Intent intent = new Intent(getBaseContext(), DisplayPrimeListActivity.class);
-                    intent.putExtra("upperLimit", upperLimit);
-                    startActivity(intent);
-                } else { // Displays alert dialog if input is empty
-                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                    builder.setTitle(R.string.alert_title).setMessage(R.string.alert_message);
-                    builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            //Automatically dismisses alert dialog
-                        }
-                    });
-                    builder.setNeutralButton(R.string.generate_random, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            Random random = new Random();
-                            //Set input to be random number between 2 and 1000002
-                            upperLimitInput.setText(String.valueOf(random.nextInt(1000000)+2));
-                            dialog.dismiss();
-                        }
-                    });
-                    builder.create().show();
+                    // Check upperLimit to make sure it's not over 15,000,000 (will result in "out
+                    // of memory" error if over)
+                    if(upperLimit > 15000000){
+                        overLimitAlert();
+                    } else {
+                        Intent intent = new Intent(getBaseContext(), DisplayPrimeListActivity.class);
+                        intent.putExtra("upperLimit", upperLimit);
+                        startActivity(intent);
+                    }
+                } else { // If input is empty display an alert
+                    emptyInputAlert();
                 }
 
             }
         });
     }
 
+    // Displays alert if user input is too high (over 15,000,000)
+    private void overLimitAlert(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(R.string.over_alert_title).setMessage(R.string.over_alert_message);
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                upperLimitInput.setText(""); // Clears input
+                dialog.dismiss();
+            }
+        });
+        builder.setNeutralButton(R.string.generate_random, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Random random = new Random();
+                //Set input to be random number between 2 and 1002
+                upperLimitInput.setText(String.valueOf(random.nextInt(1000)+2));
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+    }
+
+    // Displays alert if user input is empty; Also offers to generate random number.
+    private void emptyInputAlert(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(R.string.empty_alert_title).setMessage(R.string.empty_alert_message);
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Automatically dismisses alert dialog
+            }
+        });
+        builder.setNeutralButton(R.string.generate_random, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Random random = new Random();
+                //Set input to be random number between 2 and 1002
+                upperLimitInput.setText(String.valueOf(random.nextInt(1000)+2));
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
