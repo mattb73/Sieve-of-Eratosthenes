01990e5c28cda589eb68e4e50e6297acd475d844
Matt Bao <bobconan@gmail.com>
2015-05-11 08:52:34 -0700

    Moved the prime number generaion process to be within the constructors. This way the calls getNumberList() would be safe.

--- app/src/main/java/ucsb/mattbao/sieveoferatosthenes/PrimeGenerator.java
+++ app/src/main/java/ucsb/mattbao/sieveoferatosthenes/PrimeGenerator.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
  * a list of prime numbers up to the upper limit given (inclusive).
  * Sieve of Eratosthenes:
  * http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
- *
+ * <p/>
  * Added a field called lowerLimit so that user can specify a range
  * of prime numbers generated they want to see: [lowerLimit, upperLimit].
  */
@@ -26,6 +26,26 @@ public class PrimeGenerator {
         // 0 is not considered, mark as true (cross off).
         numberList[0] = true;
         this.primeList = new ArrayList<Integer>();
+
+        // The following for loop generates the boolean array called numberList, with all prime
+        // numbers left unmarked (false), and non-prime numbers marked true
+        for (int i = 2; i <= Math.sqrt(this.upperLimit); i++) {
+            if (!this.numberList[i]) { // If not marked
+                for (int j = i * i; j <= this.upperLimit; j += i) {
+                    if (!this.numberList[j]) { // If not marked
+                        this.numberList[j] = true;
+                    }
+                }
+            }
+        }
+        // The following for loop generates the ArrayList of prime numbers called primeList based on
+        // the numberList;
+        // Supports range: [lowerLimit, upperLimit]
+        for (int i = this.lowerLimit; i <= this.upperLimit; i++) {
+            if (!this.numberList[i]) { // If not marked true, is prime number
+                primeList.add(i);
+            }
+        }
     }
 
     // Constructor with only upper limit
@@ -37,6 +57,26 @@ public class PrimeGenerator {
         numberList[0] = true;
         numberList[1] = true;
         this.primeList = new ArrayList<Integer>();
+
+        // The following for loop generates the boolean array called numberList, with all prime
+        // numbers left unmarked (false), and non-prime numbers marked true
+        for (int i = 2; i <= Math.sqrt(this.upperLimit); i++) {
+            if (!this.numberList[i]) { // If not marked
+                for (int j = i * i; j <= this.upperLimit; j += i) {
+                    if (!this.numberList[j]) { // If not marked
+                        this.numberList[j] = true;
+                    }
+                }
+            }
+        }
+        // The following for loop generates the ArrayList of prime numbers called primeList based on
+        // the numberList;
+        // Supports range: [lowerLimit, upperLimit]
+        for (int i = this.lowerLimit; i <= this.upperLimit; i++) {
+            if (!this.numberList[i]) { // If not marked true, is prime number
+                primeList.add(i);
+            }
+        }
     }
 
     // Constructor with both upper and lower limit
@@ -48,10 +88,9 @@ public class PrimeGenerator {
         numberList[0] = true;
         numberList[1] = true;
         this.primeList = new ArrayList<Integer>();
-    }
 
-    // Returns the list of numbers as an ArrayList<Integer>
-    public ArrayList<Integer> getPrimeList() {
+        // The following for loop generates the boolean array called numberList, with all prime
+        // numbers left unmarked (false), and non-prime numbers marked true
         for (int i = 2; i <= Math.sqrt(this.upperLimit); i++) {
             if (!this.numberList[i]) { // If not marked
                 for (int j = i * i; j <= this.upperLimit; j += i) {
@@ -61,17 +100,23 @@ public class PrimeGenerator {
                 }
             }
         }
+        // The following for loop generates the ArrayList of prime numbers called primeList based on
+        // the numberList;
         // Supports range: [lowerLimit, upperLimit]
         for (int i = this.lowerLimit; i <= this.upperLimit; i++) {
             if (!this.numberList[i]) { // If not marked true, is prime number
                 primeList.add(i);
             }
         }
-        return primeList;
+    }
+
+    // Returns the list of numbers as an ArrayList<Integer>
+    public ArrayList<Integer> getPrimeList() {
+        return this.primeList;
     }
 
     // Returns numberList
-    public boolean[] getNumberList(){
+    public boolean[] getNumberList() {
         return this.numberList;
     }
 }
